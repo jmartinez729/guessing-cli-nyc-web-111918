7b0696dc7f247e1e631ed62a3ba0394ecdb24749
John Martinez <jmartinez729@gmail.com>
2018-11-09 22:34:53 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,12 +3,11 @@ def run_guessing_game
   guess = gets.chomp
   number = rand(1..6)
   
-  while guess != "exit"
-    if guess.to_i == number
-      puts "You guessed the correct number!"
-      break
-    end 
-    
+  case guess
+  when number
+    puts "You guessed the correct number!"
+    break
+  when "exit"  
     puts "Goodbye!"
     break
   end 
