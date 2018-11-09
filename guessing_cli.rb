a97911cba22ccc5ed7aec1ccdd8b28ff56ab8227
John Martinez <jmartinez729@gmail.com>
2018-11-09 22:38:11 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,6 +10,8 @@ def run_guessing_game
       break
     elsif guess.to_i == number
       puts "You guessed the correct number!"
+    elsif guess.to_i != number
+      puts "The computer guessed #{number}."
     end 
   end 
   
